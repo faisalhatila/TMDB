92874b00befa869d3946579d2b442849c90ea1f9
Faisal <hatilas1994@gmail.com>
2019-07-30 16:31:44 -0700

    Starting new TMDB project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './Components/Navbar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import BookNow from './Components/BookNow';
+import Default from './Default';
 
 function App() {
   return (
     <Fragment>
       <NavBar/>
       <Router>
-        <div>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/booknow" component={BookNow} />
-        </div>
+          <Route path="/contact" component={Contact} />
+          <Route path="/booknow" component={BookNow} />
+          <Route component={Default} />
+        </Switch>
       </Router>
     </Fragment>
   );
